2c89c77b8b1c5d8fb812ab2563c687002533e008
Abhilaksh Chauhan <abhilaksh@StarLight.local>
2026-02-05 00:51:32 +0530

    Replace uglifyjs-webpack-plugin with terser-webpack-plugin

--- ui/webpack/webpack.prod.config.js
+++ ui/webpack/webpack.prod.config.js
@@ -3,17 +3,19 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.config.js');
 
 const CompressionPlugin = require('compression-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin()],
+    },
     plugins: [
         new webpack.DefinePlugin({
             AMPLITUDE_API_KEY: JSON.stringify('8f1fcc6159b9141780ea8f019a988366'),
             EXPERIMENT_API_KEY: JSON.stringify('client-giQqL6pQjiwdW6SqxpRWrElu3LouUuSU'),
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
-        new UglifyJsPlugin(),
         new CompressionPlugin(),
     ],
 });
